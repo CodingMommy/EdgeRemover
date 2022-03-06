23c7c9cda79bf7096151c5b85c5d6c7095f346a3
Kate Davidson <95983565+CodingMommy@users.noreply.github.com>
2022-03-06 14:49:08 +0000

    First Commit

---
+++ EdgeRemover/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MainArea = New System.Windows.Forms.Panel()
+        Me.UninstallButton = New System.Windows.Forms.Button()
+        Me.CLoutput = New System.Windows.Forms.RichTextBox()
+        Me.WindowTitle = New System.Windows.Forms.Label()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.MainArea.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MainArea
+        '
+        Me.MainArea.BackColor = System.Drawing.Color.FromArgb(CType(CType(229, Byte), Integer), CType(CType(99, Byte), Integer), CType(CType(153, Byte), Integer))
+        Me.MainArea.Controls.Add(Me.WindowTitle)
+        Me.MainArea.Controls.Add(Me.CloseButton)
+        Me.MainArea.Dock = System.Windows.Forms.DockStyle.Top
+        Me.MainArea.Location = New System.Drawing.Point(0, 0)
+        Me.MainArea.Name = "MainArea"
+        Me.MainArea.Size = New System.Drawing.Size(400, 25)
+        Me.MainArea.TabIndex = 0
+        '
+        'UninstallButton
+        '
+        Me.UninstallButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(127, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.UninstallButton.FlatAppearance.BorderSize = 0
+        Me.UninstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.UninstallButton.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.UninstallButton.Location = New System.Drawing.Point(10, 209)
+        Me.UninstallButton.Margin = New System.Windows.Forms.Padding(10)
+        Me.UninstallButton.Name = "UninstallButton"
+        Me.UninstallButton.Size = New System.Drawing.Size(380, 32)
+        Me.UninstallButton.TabIndex = 1
+        Me.UninstallButton.Text = "Uninstall Edge"
+        Me.UninstallButton.UseVisualStyleBackColor = False
+        '
+        'CLoutput
+        '
+        Me.CLoutput.BackColor = System.Drawing.Color.Black
+        Me.CLoutput.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.CLoutput.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.CLoutput.ForeColor = System.Drawing.Color.White
+        Me.CLoutput.Location = New System.Drawing.Point(10, 35)
+        Me.CLoutput.Margin = New System.Windows.Forms.Padding(10)
+        Me.CLoutput.Name = "CLoutput"
+        Me.CLoutput.ReadOnly = True
+        Me.CLoutput.Size = New System.Drawing.Size(380, 164)
+        Me.CLoutput.TabIndex = 0
+        Me.CLoutput.Text = "Waiting..."
+        '
+        'WindowTitle
+        '
+        Me.WindowTitle.AutoSize = True
+        Me.WindowTitle.BackColor = System.Drawing.Color.Transparent
+        Me.WindowTitle.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.WindowTitle.ForeColor = System.Drawing.Color.White
+        Me.WindowTitle.Location = New System.Drawing.Point(3, 4)
+        Me.WindowTitle.Name = "WindowTitle"
+        Me.WindowTitle.Size = New System.Drawing.Size(115, 17)
+        Me.WindowTitle.TabIndex = 0
+        Me.WindowTitle.Text = "MSEdge Remover"
+        '
+        'CloseButton
+        '
+        Me.CloseButton.BackgroundImage = CType(resources.GetObject("CloseButton.BackgroundImage"), System.Drawing.Image)
+        Me.CloseButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.CloseButton.FlatAppearance.BorderSize = 0
+        Me.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CloseButton.Location = New System.Drawing.Point(375, 0)
+        Me.CloseButton.Margin = New System.Windows.Forms.Padding(0)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(25, 25)
+        Me.CloseButton.TabIndex = 1
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(29, Byte), Integer), CType(CType(33, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(400, 250)
+        Me.Controls.Add(Me.UninstallButton)
+        Me.Controls.Add(Me.CLoutput)
+        Me.Controls.Add(Me.MainArea)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "MSEdge Remover"
+        Me.MainArea.ResumeLayout(False)
+        Me.MainArea.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MainArea As Panel
+    Friend WithEvents WindowTitle As Label
+    Friend WithEvents CLoutput As RichTextBox
+    Friend WithEvents UninstallButton As Button
+    Friend WithEvents CloseButton As Button
+End Class
---
+++ EdgeRemover/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim parameters As CreateParams = MyBase.CreateParams
+            parameters.ClassStyle += &H20000
+            Return parameters
+        End Get
+    End Property
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Process.GetProcessesByName(Process.GetCurrentProcess.ProcessName).Count > 1 Then
+            Application.Exit()
+        End If
+        RoundedRegion(Me)
+        RoundedRegion(CLoutput)
+        RoundedRegion(UninstallButton)
+        RoundedRegion(CloseButton)
+    End Sub
+
+    Private Sub RoundedRegion(ByRef sender As Object)
+        Dim p As New Drawing2D.GraphicsPath()
+        p.StartFigure()
+        p.AddArc(New Rectangle(0, 0, 10, 10), 180, 90)
+        p.AddLine(10, 0, sender.Width - 10, 0)
+        p.AddArc(New Rectangle(sender.Width - 10, 0, 10, 10), -90, 90)
+        p.AddLine(sender.Width, 10, sender.Width, sender.Height - 10)
+        p.AddArc(New Rectangle(sender.Width - 10, sender.Height - 10, 10, 10), 0, 90)
+        p.AddLine(sender.Width - 10, sender.Height, 10, sender.Height)
+        p.AddArc(New Rectangle(0, sender.Height - 10, 10, 10), 90, 90)
+        p.CloseFigure()
+        sender.Region = New Region(p)
+    End Sub
+
+    Private IsFormBeingDragged As Boolean = False
+    Private MouseDownX As Integer
+    Private MouseDownY As Integer
+
+    Private Sub MainPanel_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MainArea.MouseDown
+        If e.Button = MouseButtons.Left Then
+            IsFormBeingDragged = True
+            Me.Opacity = 0.9
+            MouseDownX = e.X
+            MouseDownY = e.Y
+        End If
+    End Sub
+
+    Private Sub MainPanel_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MainArea.MouseUp
+        If e.Button = MouseButtons.Left Then
+            Me.Opacity = 1
+            IsFormBeingDragged = False
+        End If
+    End Sub
+
+    Private Sub MainPanel_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MainArea.MouseMove
+        If IsFormBeingDragged Then
+            Dim temp As Point = New Point()
+            temp.X = Me.Location.X + (e.X - MouseDownX)
+            temp.Y = Me.Location.Y + (e.Y - MouseDownY)
+            Me.Location = temp
+        End If
+    End Sub
+
+    Private Function GetFileVersionInfo(ByVal filename As String) As String
+        Return Version.Parse(FileVersionInfo.GetVersionInfo(filename).FileVersion).ToString
+    End Function
+
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+        Application.Exit()
+    End Sub
+
+    Dim WithEvents EdgeSetupProcess As New Process()
+
+    Private Sub UninstallButton_Click(sender As Object, e As EventArgs) Handles UninstallButton.Click
+        CLoutput.Text = ""
+        If My.Computer.FileSystem.FileExists(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & " (x86)\Microsoft\Edge\Application\msedge.exe") Then
+            Try
+                CLoutput.AppendText("Found Edge Version " & GetFileVersionInfo(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & " (x86)\Microsoft\Edge\Application\msedge.exe") & ", uninstalling..." & vbCrLf)
+                EdgeSetupProcess.StartInfo.FileName = My.Computer.FileSystem.SpecialDirectories.ProgramFiles & " (x86)\Microsoft\Edge\Application\" & GetFileVersionInfo(My.Computer.FileSystem.SpecialDirectories.ProgramFiles & " (x86)\Microsoft\Edge\Application\msedge.exe") & "\Installer\setup.exe"
+                EdgeSetupProcess.StartInfo.Arguments = "--uninstall --force-uninstall --system-level"
+                EdgeSetupProcess.Start()
+                EdgeSetupProcess.WaitForExitAsync()
+            Catch ex As Exception
+                CLoutput.AppendText("Unhandled exception: " & ex.ToString & vbCrLf)
+            End Try
+        Else
+            CLoutput.AppendText("Unable to find msedge.exe, have you already uninstalled Edge?" & vbCrLf)
+        End If
+    End Sub
+
+
+
+    Private Sub CLoutput_TextChanged(sender As Object, e As EventArgs) Handles CLoutput.TextChanged
+        CLoutput.SelectionStart = CLoutput.Text.Length
+        CLoutput.ScrollToCaret()
+    End Sub
+
+    Private Sub EdgeSetupProcess_OutputDataReceived(sender As Object, e As EventArgs) Handles EdgeSetupProcess.Exited
+        CLoutput.Invoke(Sub() CLoutput.AppendText("Succesfully uninstalled MSEdge..." & vbCrLf))
+    End Sub
+End Class
